cf4892ec429c8610bd4324b482662c77827378ea
Noelia Mendoza <nkmendoza@espe.edu.ec>
2026-06-09 10:24:28 -0500

    Lab 4

--- practica_react/src/services/rick-and-morty-service.js
+++ practica_react/src/services/rick-and-morty-service.js
@@ -1,5 +1,7 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const obtenerPersonajes = async () => {
-    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const response = await fetch(API_URL);
     const data = await response.json();
     return data.results;
 };
